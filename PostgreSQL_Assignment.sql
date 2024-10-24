733b497fef16ef9de5c19aec99ec266f6b083639
Alauddin-24434 <muhammadalauddin24434@gmail.com>
2024-10-24 11:33:50 +0600

    add query

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -37,36 +37,36 @@ CREATE TABLE enrollment (
 -- Sample Data Insertion
 
 -- Insert sample data into "students" table
-INSERT INTO students (student_id, student_name, age, email, frontend_mark, backend_mark, status) 
+INSERT INTO students (student_name, age, email, frontend_mark, backend_mark, status) 
 VALUES 
-(1, 'Sameer', 21, 'sameer@example.com', 48, 60, NULL),
-(2, 'Zoya', 23, 'zoya@example.com', 52, 58, NULL),
-(3, 'Nabil', 22, 'nabil@example.com', 37, 46, NULL),
-(4, 'Rafi', 24, 'rafi@example.com', 41, 40, NULL),
-(5, 'Sophia', 22, 'sophia@example.com', 50, 52, NULL),
-(6, 'Hasan', 23, 'hasan@gmail.com', 43, 39, NULL);
+('Sameer', 21, 'sameer@example.com', 48, 60, NULL),
+('Zoya', 23, 'zoya@example.com', 52, 58, NULL),
+('Nabil', 22, 'nabil@example.com', 37, 46, NULL),
+('Rafi', 24, 'rafi@example.com', 41, 40, NULL),
+('Sophia', 22, 'sophia@example.com', 50, 52, NULL),
+('Hasan', 23, 'hasan@gmail.com', 43, 39, NULL);
 
 -- Insert sample data into "courses" table
-INSERT INTO courses (course_id, course_name, credits) 
+INSERT INTO courses (course_name, credits) 
 VALUES 
-(1, 'Next.js', 3),
-(2, 'React.js', 4),
-(3, 'Databases', 3),
-(4, 'Prisma', 3);
+('Next.js', 3),
+('React.js', 4),
+('Databases', 3),
+('Prisma', 3);
 
 -- Insert sample data into "enrollment" table
-INSERT INTO enrollment (enrollment_id, student_id, course_id) 
+INSERT INTO enrollment (student_id, course_id) 
 VALUES 
-(1, 1, 1),
-(2, 1, 2),
-(3, 2, 1),
-(4, 3, 2);
+(1, 1),
+(1, 2),
+(2, 1),
+(3, 2);
 
 -- Queries
 
 -- Query 1: Insert a new student record
 INSERT INTO students (student_name, age, email, frontend_mark, backend_mark, status) 
-VALUES ('Alauddin', 24, 'muhammadalauddin24434@gmail.com', 52, 58, NULL);
+VALUES ('Alauddin', 24, 'muhammadalauddin24434@gmail.com', 50, 58, NULL);
 
 -- Query 2: Retrieve students enrolled in 'Next.js'
 SELECT student_name 
